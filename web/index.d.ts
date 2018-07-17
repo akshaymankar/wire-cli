3a4570fee7d401fbc66001f8a589009697116f5a
Benny Neugebauer <bn@bennyn.de>
2018-07-17 14:28:55 +0200

    refactor(web): Replace JSON output with JS output (#27)

--- web/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { Root } from 'protobufjs';
-declare const loadProtocolBuffers: () => Root;
-export default loadProtocolBuffers;
--- web/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var protobufjs_1 = require("protobufjs");
-var protoJSON = require('./messages.json');
-var loadProtocolBuffers = function () { return protobufjs_1.Root.fromJSON(protoJSON); };
-exports.default = loadProtocolBuffers;
