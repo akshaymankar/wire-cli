9369fa440d43e23ec5fa3f1b14893e714c83adce
Akshay Mankar <akshay@wire.com>
2020-05-08 11:41:15 +0200

    Use polysemy-mocks to generate mocks

--- test/Wire/CLI/ExecuteSpec.hs
+++ test/Wire/CLI/ExecuteSpec.hs
@@ -1,9 +1,16 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
 module Wire.CLI.ExecuteSpec where
 
 import qualified Network.URI as URI
 import Polysemy
 import qualified System.CryptoBox as CBox
 import Test.Hspec
+import Test.Polysemy.Mock
+import Test.Polysemy.Mock.TH
 import Test.QuickCheck
 import Wire.CLI.Backend (Backend)
 import qualified Wire.CLI.Backend as Backend
@@ -11,20 +18,14 @@ import Wire.CLI.Backend.Arbitrary ()
 import qualified Wire.CLI.Backend.Client as Client
 import Wire.CLI.CryptoBox (CryptoBox)
 import qualified Wire.CLI.Execute as Execute
-import qualified Wire.CLI.MockBackend as MockBackend
-import Wire.CLI.MockBackend (MockBackend)
-import Wire.CLI.MockCryptoBox (MockCryptoBox)
-import qualified Wire.CLI.MockCryptoBox as MockCryptoBox
-import qualified Wire.CLI.MockStore as MockStore
-import Wire.CLI.MockStore (MockStore)
 import qualified Wire.CLI.Options as Opts
 import Wire.CLI.Store (Store)
 
-withMocks ::
-  Members '[MockCryptoBox, MockStore, MockBackend, Embed IO] r =>
-  Sem (Backend ': (Store ': (CryptoBox ': r))) a ->
-  Sem r a
-withMocks = MockCryptoBox.mock . MockStore.mock . MockBackend.mock
+genMock ''Backend
+
+genMock ''Store
+
+genMock ''CryptoBox
 
 {-# ANN spec ("HLint: ignore Redundant do" :: String) #-}
 spec :: Spec
@@ -33,32 +34,33 @@ spec = do
     it "should login and store creds" testLoginAndStore
 
 testLoginAndStore :: IO ()
-testLoginAndStore = runM . MockBackend.run . MockStore.run . MockCryptoBox.run $ do
+testLoginAndStore = runM . evalMocks @'[Backend, Store, CryptoBox] $ do
   let Just server = URI.parseURI "https://be.example.com"
   let loginOpts = Opts.LoginOptions server "handle" "pwwpw"
   let loginCommand = Opts.Login loginOpts
   cred <- embed $ generate arbitrary
   prekey <- embed $ generate arbitrary
-  MockBackend.mockLoginReturns (const $ Backend.LoginSuccess cred)
-  MockCryptoBox.mockNewPrekeyReturns (const $ CBox.Success prekey)
+  mockLoginReturns (const $ pure $ Backend.LoginSuccess cred)
+  mockNewPrekeyReturns (const . pure $ CBox.Success prekey)
   -- execute the command
-  withMocks $ Execute.execute loginCommand
+  mockMany @'[Backend, Store, CryptoBox] $ Execute.execute loginCommand
+  -- withMocks $ Execute.execute loginCommand
   -- expectations
   -- Call backend
-  loginCalls <- MockBackend.mockLoginCalls
-  embed $ loginCalls `shouldBe` [loginOpts]
+  loginCalls' <- mockLoginCalls
+  embed $ loginCalls' `shouldBe` [loginOpts]
   -- Store creds
-  saveCredsCalls <- MockStore.mockSaveCredsCalls
-  embed $ saveCredsCalls `shouldBe` [cred]
+  saveCredsCalls' <- mockSaveCredsCalls
+  embed $ saveCredsCalls' `shouldBe` [cred]
   -- Register Client
   -- Generate 100 prekeys
-  newPrekeyCalls <- MockCryptoBox.mockNewPrekeyCalls
-  embed $ length newPrekeyCalls `shouldBe` 101
-  embed $ newPrekeyCalls `shouldBe` ([0 .. 99] <> [maxBound])
+  newPrekeyCalls' <- mockNewPrekeyCalls
+  embed $ length newPrekeyCalls' `shouldBe` 101
+  embed $ newPrekeyCalls' `shouldBe` ([0 .. 99] <> [maxBound])
   -- Make register call to backend
-  registerClientCalls <- MockBackend.mockRegisterClientCalls
-  embed $ length registerClientCalls `shouldBe` 1
-  let (registerCred, uri, newClient) = head registerClientCalls
+  registerClientCalls' <- mockRegisterClientCalls
+  embed $ length registerClientCalls' `shouldBe` 1
+  let (registerCred, uri, newClient) = head registerClientCalls'
   embed $ registerCred `shouldBe` cred
   embed $ uri `shouldBe` server
   embed $ Client.cookie newClient `shouldBe` "wire-cli-cookie-label"
