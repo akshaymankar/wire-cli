4f9e752ee5d8c019aa7b740f2c0d68eecce90954
Florian Imdahl <ffflorian@users.noreply.github.com>
2020-12-21 14:48:16 +0100

    feat: Add backend-api-protobuf (SQCORE-267) (#66)

--- web/compile.js
+++ web/compile.js
@@ -21,14 +21,19 @@ const fs = require('fs');
 const path = require('path');
 const {pbjs, pbts} = require('protobufjs/cli');
 
-const dtsOutput = path.join(__dirname, 'Protobuf.d.ts');
-const jsOutput = path.join(__dirname, 'Protobuf.js');
-const protoBufferFile = path.join(__dirname, '..', 'proto', 'messages.proto');
+const protoDir = path.join(__dirname, '../proto');
+const protoFiles = fs.readdirSync(protoDir).filter(fileName => fileName.endsWith('.proto'));
 
-pbjs.main(['--target', 'static-module', '--wrap', 'commonjs', protoBufferFile], (error, output) => {
-  if (error) {
-    throw error;
-  }
-  fs.writeFileSync(jsOutput, output, {encoding: 'utf8'});
-  pbts.main(['--out', dtsOutput, '--no-comments', jsOutput]);
-});
+for (const protoFileName of protoFiles) {
+  const protoBufferFile = path.join(__dirname, '../proto', protoFileName);
+  const dtsOutput = path.join(__dirname, protoFileName.replace(/\.proto$/, '.d.ts'));
+  const jsOutput = path.join(__dirname, protoFileName.replace(/\.proto$/, '.js'));
+
+  pbjs.main(['--target', 'static-module', '--wrap', 'commonjs', protoBufferFile], (error, output) => {
+    if (error) {
+      throw error;
+    }
+    fs.writeFileSync(jsOutput, output, {encoding: 'utf8'});
+    pbts.main(['--out', dtsOutput, '--no-comments', jsOutput]);
+  });
+}
