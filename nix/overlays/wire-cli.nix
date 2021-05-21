aec80929db6b2d18eed69f5948288eef42246791
Akshay Mankar <itsakshaymankar@gmail.com>
2021-05-21 08:35:54 +0200

    Bump niv and nixpkgs

--- nix/overlays/wire-cli.nix
+++ nix/overlays/wire-cli.nix
@@ -13,7 +13,7 @@ self: super: {
           rev = "4067ad96b125942545dbdec8c1a89f1e1b65d013";
           sha256 = "1i9dlhw0xk1viglyhail9fb36v1awrypps8jmhrkz8k1bhx98ci3";
         };
-        cargoSha256 = "0zs8ibv7rinrrzp9naxd7yak7kn1gp3pjb3g8i4wf7xw2hkkq81z";
+        cargoSha256 = "123jza3awrixgm35la3y3fqsixp7q2b0jpnp809jq34p215ggyh1";
         postInstall = ''
           mkdir -p $out/include
           cp src/cbox.h $out/include
