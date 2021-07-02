1da64d2bfac30bd919ac8dce97f383522cd3a87b
Akshay Mankar <itsakshaymankar@gmail.com>
2021-07-02 14:35:42 +0200

    GUI.Worker: Auto-refresh token and retry

--- src/Wire/GUI/Worker.hs
+++ src/Wire/GUI/Worker.hs
@@ -10,13 +10,16 @@ import Polysemy.Random (Random)
 import qualified Polysemy.Random as Random
 import qualified System.CryptoBox as CBox
 import Wire.CLI.Backend (Backend)
+import qualified Wire.CLI.Backend as Backend
 import qualified Wire.CLI.Backend.HTTP as HTTPBackend
 import Wire.CLI.CryptoBox (CryptoBox)
 import qualified Wire.CLI.CryptoBox.FFI as CryptoBoxFFI
 import Wire.CLI.Error (WireCLIError)
+import qualified Wire.CLI.Error as WireCLIError
 import Wire.CLI.Execute (execute)
 import qualified Wire.CLI.Options as Opts
 import Wire.CLI.Store (Store)
+import qualified Wire.CLI.Store as Store
 import qualified Wire.CLI.Store.File as FileStore
 import Wire.CLI.UUIDGen (UUIDGen)
 import qualified Wire.CLI.UUIDGen as UUIDGen
@@ -32,22 +35,43 @@ worker :: HTTP.Manager -> FilePath -> CBox.Box -> OutChan Work -> IO ()
 worker mgr storePath cbox workChan = go
   where
     runAllEffects :: Sem AllEffects a -> IO (Either WireCLIError a)
-    runAllEffects =
+    runAllEffects action = do
       runFinal
         . embedToFinal
-        . Error.runError -- TODO: log the error!
+        . Error.errorToIOFinal -- TODO: log the error!
         . UUIDGen.run
         . Random.runRandomIO
         . HTTPBackend.run "wire-cli-label" mgr
         . FileStore.run storePath
         . CryptoBoxFFI.run cbox
+        $ action
+
+    runAllEffectsWithRetry :: Sem AllEffects a -> IO (Either WireCLIError a)
+    runAllEffectsWithRetry action = do
+      eitherRes <- runAllEffects action
+      case eitherRes of
+        Left WireCLIError.Http401 ->
+          runAllEffects $ refreshTokenAndSave >> action
+        x -> pure x
+
     go :: IO ()
     go = do
       putStrLn "Waiting For work!"
       Work action callback <- Unagi.readChan workChan
       putStrLn "Got work!"
-      callback =<< runAllEffects action
+      callback =<< runAllEffectsWithRetry action
       go
+
+-- TODO: Unit test
+refreshTokenAndSave :: Members '[Backend, Store, Error WireCLIError, Embed IO] r => Sem r ()
+refreshTokenAndSave = do
+  embed $ putStrLn "refreshing token!"
+  creds <- Store.getCreds >>= Error.note WireCLIError.NotLoggedIn
+  let server = Backend.server creds
+      cookies = Backend.credentialCookies $ Backend.credential creds
+  token <- Backend.refreshToken server cookies
+  let serverCred = Backend.ServerCredential server (Backend.Credential cookies token)
+  Store.saveCreds serverCred
 
 -- | Blocks until work is done. This is required so UI code isn't cluttered with
 -- information on how to resolve all the effects.
