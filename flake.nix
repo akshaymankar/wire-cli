a2fcddaa53208153508ef6da71dfe11ea5867809
Akshay Mankar <itsakshaymankar@gmail.com>
2022-04-29 11:49:03 +0200

    Use haskellPackages.shellFor to get dev shell
    This commit also removes a lot of cabal code which was also pinning and
    constraining packages. All the pinning and constraining is now done in nixpkgs.
    Doing this ensures that cabal doesn't try to build anything and just trusts the
    ghc-pkg provided by nix to be the packages it needs.
    
    Unfortunately, this drops support for being able to run builds on a non-nix
    system. If it becomes a problem, maybe a script can be written to generate a
    cabal.project and a cabal.project.freeze to ensure builds run. But that is a
    problem to be solved when it becomes one.

--- flake.nix
+++ flake.nix
@@ -11,171 +11,88 @@
           inherit system;
           overlays = [(import ./nix/overlays/wire-cli.nix)];
         };
-
-        c-lib-out-deps = [
-            # Core
-            pkgs.cryptobox
-            pkgs.openssl.out
-            pkgs.zlib.out
-
-            # For wire-api
-            pkgs.lzma.out
-
-            # GUI
-            pkgs.gtk4.out
-            pkgs.pango.out
-            pkgs.graphene
-            pkgs.gdk-pixbuf.out
-            pkgs.atk.out
-            pkgs.harfbuzz.out
-        ];
-
-        compile-deps = pkgs.buildEnv {
-          name = "wire-server-compile-deps";
-          paths = [
-            pkgs.bash
-            pkgs.coreutils
-            pkgs.gnused
-            pkgs.gnugrep
-            pkgs.pkgconfig
-            pkgs.gawk
-            pkgs.git
-
-            pkgs.haskell.compiler.ghc8107
-
-            # For cabal
-            pkgs.pkgconfig
-            pkgs.binutils
-
-            # Core
-            pkgs.openssl.dev
-            pkgs.zlib.dev
-
-            # For wire-api
-            pkgs.lzma.dev
-
-            # hsaml2
-            pkgs.libxml2.dev
-
-            # Message
-            pkgs.protobuf
-            pkgs.haskellPackages.proto-lens-protoc
-
-            # GUI
-            pkgs.pcre.dev
-            pkgs.libffi.dev
-            pkgs.gobject-introspection.dev
-            pkgs.glib.dev
-            pkgs.pango.dev
-            pkgs.gtk4.dev
-            pkgs.gdk-pixbuf.dev
-            pkgs.cairo.dev
-            pkgs.atk.dev
-            pkgs.harfbuzz.dev
-            pkgs.vulkan-loader.dev
-          ] ++ c-lib-out-deps;
+        hlib = pkgs.haskell.lib;
+        # Avoids unnecessary recompiles
+        filteredSource = pkgs.lib.cleanSourceWith {
+          src = ./.;
+          filter = path: type:
+            let baseName = baseNameOf (toString path);
+            in pkgs.lib.cleanSourceFilter path type && !(
+              baseName == "wire-message" ||
+              baseName == "flake.nix" ||
+              baseName == "flake.lock" ||
+              baseName == "dist-newstyle" ||
+              baseName == "hack" ||
+              baseName == "nix" ||
+              baseName == ".hls-env" ||
+              baseName == "Makefile" ||
+              builtins.match "^cabal\.project\..*$" baseName != null ||
+              baseName == "lsp-wrapper.sh" ||
+              baseName == "hack" ||
+              baseName == ".envrc" ||
+              baseName == "hie.yaml" ||
+              baseName == ".hlint.yaml" ||
+              baseName == ".hspec" ||
+              baseName == "pins.yaml" ||
+              baseName == "ci"
+            );
         };
-
-        # This performs roughly the same setup as direnv's load_prefix function, but
-        # only when invoking cabal. This means that we can set LD_LIBRARY_PATH just
-        # for cabal, as setting it in direnv can interfere with programs in the host
-        # system, especially for non-NixOS users.
-        cabal-wrapper = pkgs.writeShellScriptBin "cabal" ''
-          export CPATH="${compile-deps}/include"
-          export LD_LIBRARY_PATH="${compile-deps}/lib"
-          export LIBRARY_PATH="${compile-deps}/lib"
-          export PKG_CONFIG_PATH="${compile-deps}/lib/pkgconfig"
-          export PATH="${compile-deps}/bin"
-          export CONFIG_SHELL="${compile-deps}/bin/sh"
-
-          export XDG_DATA_DIRS="${compile-deps}/share"
-          export GI_TYPELIB_PATH="${compile-deps}/lib/girepository-1.0"
-
-          exec "${pkgs.haskellPackages.cabal-install-head}/bin/cabal" "$@"
-        '';
-
+        haskellPackages = pkgs.haskell.packages.ghc8107.override {
+          overrides = hself: hsuper:
+            let generated = import ./nix/haskell-overrides/overrides.nix hself hsuper;
+                manual = {
+                  wire-message =
+                    let basic = hself.callPackage ./wire-message/default.nix {};
+                    in hlib.addBuildTool basic pkgs.protobuf;
+                  wire-cli = hlib.overrideSrc (hself.callPackage ./default.nix {}) {src = filteredSource;};
+                  network-arbitrary = hlib.markUnbroken (hlib.doJailbreak hsuper.network-arbitrary);
+                  polysemy-zoo = hlib.markUnbroken (hlib.doJailbreak hsuper.polysemy-zoo);
+                  cql = hlib.markUnbroken hsuper.cql;
+                  cql-io = hlib.dontCheck hsuper.cql-io;
+                  lens-datetime = hlib.markUnbroken (hlib.doJailbreak hsuper.lens-datetime);
+                  wai-predicates = hlib.markUnbroken hsuper.wai-predicates;
+                  gi-gio-hs-list-model = hlib.markUnbroken hsuper.gi-gio-hs-list-model;
+                  gi-gtk = hsuper.gi-gtk_4_0_5;
+                  gi-gdk = hsuper.gi-gdk_4_0_4;
+                  gi-graphene = hlib.markUnbroken (hlib.addPkgconfigDepend hsuper.gi-graphene pkgs.graphene);
+                  http-client-websockets = hlib.markUnbroken (hlib.dontCheck hsuper.http-client-websockets);
+                  polysemy = hsuper.polysemy_1_7_1_0;
+                  polysemy-plugin = hsuper.polysemy-plugin_0_4_3_0;
+                  hsaml2 = hlib.dontCheck generated.hsaml2;
+                  universe-base = hlib.addExtraLibrary hsuper.universe-base hsuper.OneTuple;
+                  wire-message-proto-lens = hlib.addBuildTool generated.wire-message-proto-lens pkgs.protobuf;
+                  hscim = hlib.doJailbreak generated.hscim;
+                  bytestring-arbitrary = hlib.markUnbroken (hlib.doJailbreak hsuper.bytestring-arbitrary);
+                  servant-server = hlib.dontCheck hsuper.servant-server;
+                  singeltons = hlib.doJailbreak generated.singletons;
+                  saml2-web-sso = hlib.dontCheck generated.saml2-web-sso;
+                };
+            in generated // manual;
+        };
       in rec {
-        packages.dev-env = pkgs.buildEnv {
-          name = "wire-clie-build-env";
-          paths = [
-            # Tools
-            pkgs.haskell.compiler.ghc8107
-            cabal-wrapper
-            pkgs.gnumake
-            pkgs.haskell.packages.ghc8107.haskell-language-server
-            pkgs.haskellPackages.cabal-plan
-            pkgs.jq
-            pkgs.haskellPackages.hspec-discover # So, HLS can find it.
+        packages = {
+          wire-cli = haskellPackages.wire-cli;
+          dev-shell = haskellPackages.shellFor {
+            packages = p: [p.wire-cli p.wire-message];
+            buildInputs = [
+              # Tools
+              pkgs.cabal-install
+              pkgs.gnumake
+              pkgs.haskell.packages.ghc8107.haskell-language-server
+              pkgs.haskellPackages.cabal-plan
+              pkgs.jq
 
-            # cabal2nix
-            pkgs.cabal2nix
-            pkgs.yq
+              # cabal2nix
+              pkgs.cabal2nix
+              pkgs.yq
 
-            # concourse
-            pkgs.fly
-            pkgs.dhall-json
-          ] ++ c-lib-out-deps;
+              # concourse
+              pkgs.fly
+              pkgs.dhall-json
+            ];
+          };
         };
-        packages.wire-cli =
-          let hlib = pkgs.haskell.lib;
-              # Avoids unnecessary recompiles
-              filteredSource = pkgs.lib.cleanSourceWith {
-                src = ./.;
-                filter = path: type:
-                  let baseName = baseNameOf (toString path);
-                  in pkgs.lib.cleanSourceFilter path type && !(
-                    baseName == "wire-message" ||
-                    baseName == "flake.nix" ||
-                    baseName == "flake.lock" ||
-                    baseName == "dist-newstyle" ||
-                    baseName == "hack" ||
-                    baseName == "nix" ||
-                    baseName == ".hls-env" ||
-                    baseName == "Makefile" ||
-                    builtins.match "^cabal\.project\..*$" baseName != null ||
-                    baseName == "lsp-wrapper.sh" ||
-                    baseName == "hack" ||
-                    baseName == ".envrc" ||
-                    baseName == "hie.yaml" ||
-                    baseName == ".hlint.yaml" ||
-                    baseName == ".hspec" ||
-                    baseName == "pins.yaml" ||
-                    baseName == "ci"
-                  );
-              };
-              haskellPackages = pkgs.haskell.packages.ghc8107.override {
-                overrides = hself: hsuper:
-                  let generated = import ./nix/haskell-overrides/overrides.nix hself hsuper;
-                      manual = {
-                        wire-message =
-                          let basic = hself.callPackage ./wire-message/default.nix {};
-                          in hlib.addBuildTool basic pkgs.protobuf;
-                        wire-cli = hlib.overrideSrc (hself.callPackage ./default.nix {}) {src = filteredSource;};
-                        network-arbitrary = hlib.markUnbroken (hlib.doJailbreak hsuper.network-arbitrary);
-                        polysemy-zoo = hlib.markUnbroken (hlib.doJailbreak hsuper.polysemy-zoo);
-                        cql = hlib.markUnbroken hsuper.cql;
-                        cql-io = hlib.dontCheck hsuper.cql-io;
-                        lens-datetime = hlib.markUnbroken (hlib.doJailbreak hsuper.lens-datetime);
-                        wai-predicates = hlib.markUnbroken hsuper.wai-predicates;
-                        gi-gio-hs-list-model = hlib.markUnbroken hsuper.gi-gio-hs-list-model;
-                        gi-gtk = hsuper.gi-gtk_4_0_5;
-                        gi-gdk = hsuper.gi-gdk_4_0_4;
-                        gi-graphene = hlib.markUnbroken (hlib.addPkgconfigDepend hsuper.gi-graphene pkgs.graphene);
-                        http-client-websockets = hlib.markUnbroken (hlib.dontCheck hsuper.http-client-websockets);
-                        polysemy = hsuper.polysemy_1_7_1_0;
-                        polysemy-plugin = hsuper.polysemy-plugin_0_4_3_0;
-                        hsaml2 = hlib.dontCheck generated.hsaml2;
-                        universe-base = hlib.addExtraLibrary hsuper.universe-base hsuper.OneTuple;
-                        wire-message-proto-lens = hlib.addBuildTool generated.wire-message-proto-lens pkgs.protobuf;
-                        hscim = hlib.doJailbreak generated.hscim;
-                        bytestring-arbitrary = hlib.markUnbroken (hlib.doJailbreak hsuper.bytestring-arbitrary);
-                        servant-server = hlib.dontCheck hsuper.servant-server;
-                        singeltons = hlib.doJailbreak generated.singletons;
-                        saml2-web-sso = hlib.dontCheck generated.saml2-web-sso;
-                      };
-                  in generated // manual;
-              };
-          in haskellPackages.wire-cli;
-        defaultPackage = packages.dev-env;
-    });
+        defaultPackage = packages.wire-cli;
+        devShell = packages.dev-shell;
+      });
 }
--- lsp-wrapper.sh
+++ lsp-wrapper.sh
@@ -3,11 +3,4 @@
 set -euo pipefail
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-nix build -o "$SCRIPT_DIR/.hls-env" "$SCRIPT_DIR"
-
-eval "$(direnv stdlib)"
-env="$SCRIPT_DIR/.hls-env"
-load_prefix "${env}"
-
-haskell-language-server-wrapper "$@"
+direnv exec "$SCRIPT_DIR" haskell-language-server-wrapper "$@"
